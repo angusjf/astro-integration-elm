f16893a2d7d7310ee0ac4a04725b2f02ad8650cd
Angus Findlay <angus.findlay@icloud.com>
2022-12-05 21:18:29 +0000

    elm compatible dev server

--- src/index.ts
+++ src/index.ts
@@ -2,45 +2,72 @@ import { AstroIntegration } from "astro";
 import * as elm from 'node-elm-compiler';
 import { toESModule } from 'elm-esm';
 
+// from 'node-elm-compiler'
+type ElmCompilerOption = 
+  | 'spawn'
+  | 'cwd'
+  | 'pathToElm'
+  | 'help'
+  | 'output'
+  | 'report'
+  | 'debug'
+  | 'verbose'
+  | 'processOpts'
+  | 'docs'
+  | 'optimize'
+
+type CompilerOptions = Partial<Record<ElmCompilerOption, unknown>>
+
 export default function (): AstroIntegration {
   return {
-    name: "elm",
+    name: "elm-astro-integration",
     hooks: {
+      "astro:server:setup": (options) => {
+        options.server.middlewares.use((req, res, next) => {
+          if (req.originalUrl.endsWith('.elm')) {
+            const filename = (req.originalUrl as string).replace("/@fs", "")
+            const compiled = compile(filename)
+            res.statusCode = 200
+            res.setHeader('Content-Type', 'application/javascript')
+            res.end(compiled)
+          } else {
+            next()
+          }
+        })
+      },
       "astro:config:setup": ({ addRenderer, updateConfig }) => {
         addRenderer({
-          name: "elm",
+          name: "elm-astro-integration",
           serverEntrypoint: "./elm-integration/elm-server.js",
           clientEntrypoint: "./elm-integration/elm-client.js",
         });
         updateConfig({
-          vite: { plugins: [elmPlugin()] }
+          vite: { plugins: [elmPlugin] }
         });
       },
     },
   };
 }
 
-function elmPlugin(compilerOptions = {}) {
-  return {
-    name: 'elm',
-    enforce: 'pre',
-    api: {},
-    async load(id, _) {
-      if (!id.endsWith('.elm')) return;
+const elmPlugin = {
+  name: 'vite-plugin-elm',
+  transform(code, id, _: unknown) {
+    if (!id.endsWith('.elm')) return;
+    return compile(id)
+  }
+}
 
-      const out = toESModule(elm.compileToStringSync(id, compilerOptions));
-
-      return `
-      try {
-        global.document = {}
-        global.XMLHttpRequest = {}
-      } catch (e) {}
-      ${out}      
-      export default {
-        $$elm: true,
-        ...Elm[Object.keys(Elm)[0]]
-      }
-      `
+function compile(filename) {
+    const out = toESModule(elm.compileToStringSync(filename, {}));
+    return `
+    try {
+      global.document = {}
+      global.XMLHttpRequest = {}
+    } catch (e) {}
+    ${out}      
+    export default {
+      $$elm: true,
+      ...Elm[Object.keys(Elm)[0]]
     }
-  };
+    `
 }
