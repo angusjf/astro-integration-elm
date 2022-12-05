9f8b8b439c193044d17f5e5d4a540f497593cb91
Angus Findlay <angus.findlay@icloud.com>
2022-12-05 16:03:47 +0000

    cleaned up the repo to contain only the integration

--- astro.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "astro/config";
-import elmIntegration from "./elm-integration";
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [elmIntegration()],
-});
